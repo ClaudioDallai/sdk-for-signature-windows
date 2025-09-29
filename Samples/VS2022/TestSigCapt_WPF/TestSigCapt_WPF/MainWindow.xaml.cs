7d65315591bb30b43cb80df1a7a5449bdfd418fd
Claudio Dallai <149816791+ClaudioDallai@users.noreply.github.com>
2025-09-29 16:51:51 +0200

    tests re-using iTextSharp and BouncyCastle to load a certificate

--- Samples/VS2022/TestSigCapt_WPF/TestSigCapt_WPF/MainWindow.xaml.cs
+++ Samples/VS2022/TestSigCapt_WPF/TestSigCapt_WPF/MainWindow.xaml.cs
@@ -12,25 +12,32 @@
 // Wacom Ink sdk
 using FlSigCaptLib;
 using FLSIGCTLLib;
-
-// C# framework
-using System;
-using System.IO;
-using System.Windows;
-using PdfSharp.Pdf.Security;
-using System.Diagnostics;
-using System.Text;
-using System.Windows.Media.Imaging;
-using System.Collections.Generic;
-using System.Windows.Controls;
-using System.Data.Odbc;
-using System.Security.Cryptography.Pkcs;
-
-// PDF manipulation
-using PdfPig = UglyToad.PdfPig;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using iTextSharp.text.pdf.security;
+using Microsoft.Win32;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Pkcs;
+using PdfSharp.Drawing;
 using PdfSharp.Pdf;
 using PdfSharp.Pdf.IO;
-using PdfSharp.Drawing;
+using PdfSharp.Pdf.Security;
+// C# framework
+using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography.Pkcs;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+// PDF manipulation
+using PdfPig = UglyToad.PdfPig;
 
 
 namespace TestSigCapt_WPF
@@ -50,69 +57,72 @@ namespace TestSigCapt_WPF
         {
 
 
-            #region Signature INK sdk and PDF Manipulation (MIT)
+            #region Signature INK sdk and PDF Manipulation
 
-            //String signTargetPath = "";
 
-            //print("btnSign was pressed");
-            //SigCtl sigCtl = new SigCtl();
-            //sigCtl.Licence = _sdkLicenseKey;
-            ////DynamicCapture dc = new DynamicCaptureClass();
-            //DynamicCapture dc = new FlSigCaptLib.DynamicCapture();
-            //DynamicCaptureResult res = dc.Capture(sigCtl, "Mario Rossi", "Presentazione Esempio 1", null, null);
-            //if (res == DynamicCaptureResult.DynCaptOK)
-            //{
-            //    print("signature captured successfully");
-            //    SigObj sigObj = (SigObj)sigCtl.Signature;
-            //    sigObj.set_ExtraData("AdditionalData", "C# test: Additional data");
+            String signTargetPath = "";
 
-            //    //var testRead = sigObj.ExtraData["AdditionalData"]; // Works
+            print("btnSign was pressed");
+            SigCtl sigCtl = new SigCtl();
+            sigCtl.Licence = _sdkLicenseKey;
+            //DynamicCapture dc = new DynamicCaptureClass();
+            DynamicCapture dc = new FlSigCaptLib.DynamicCapture();
+            DynamicCaptureResult res = dc.Capture(sigCtl, "Mario Rossi", "Presentazione Esempio 1", null, null);
+            if (res == DynamicCaptureResult.DynCaptOK)
+            {
+                print("signature captured successfully");
+                SigObj sigObj = (SigObj)sigCtl.Signature;
+                sigObj.set_ExtraData("AdditionalData", "C# test: Additional data");
 
-            //    String dateStr = DateTime.Now.ToString("hhmmss");
+                //var testRead = sigObj.ExtraData["AdditionalData"]; // Works
 
-            //    string folderPath = @"C:\temp";
-            //    if (!Directory.Exists(folderPath))
-            //    {
-            //        Directory.CreateDirectory(folderPath);
-            //    }
+                String dateStr = DateTime.Now.ToString("hhmmss");
 
-            //    signTargetPath = @"C:\Users\visio\Desktop\PDF_ManipulationTests\Signs\" + dateStr + ".png";
-            //    print("Outputting to file " + signTargetPath);
-            //    try
-            //    {
-            //        //print("Saving signature to file " + filename);
-            //        // Need to understand if DimensionX and DimensionY alter the image metadata. These values (200, 150) were already here in the example.
-            //        sigObj.RenderBitmap(signTargetPath, 150, 100, "image/png", 0.5f, 0xff0000, 0xffffff, 10.0f, 10.0f, RBFlags.RenderOutputFilename | RBFlags.RenderColor32BPP | RBFlags.RenderEncodeData);
+                string folderPath = @"C:\temp";
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
 
-            //        print("Loading image from " + signTargetPath);
-            //        BitmapImage src = new BitmapImage();
-            //        src.BeginInit();
-            //        src.UriSource = new Uri(signTargetPath, UriKind.Absolute);
-            //        src.EndInit();
+                signTargetPath = @"C:\Users\visio\Desktop\PDF_ManipulationTests\Signs\" + dateStr + ".png";
+                print("Outputting to file " + signTargetPath);
+                try
+                {
+                    //print("Saving signature to file " + filename);
+                    // Need to understand if DimensionX and DimensionY alter the image metadata. These values (200, 150) were already here in the example.
+                    sigObj.RenderBitmap(signTargetPath, 150, 100, "image/png", 0.5f, 0xff0000, 0xffffff, 10.0f, 10.0f, RBFlags.RenderOutputFilename | RBFlags.RenderColor32BPP | RBFlags.RenderEncodeData);
 
-            //        imgSig.Source = src;
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        MessageBox.Show(ex.Message);
-            //    }
+                    print("Loading image from " + signTargetPath);
+                    BitmapImage src = new BitmapImage();
+                    src.BeginInit();
+                    src.UriSource = new Uri(signTargetPath, UriKind.Absolute);
+                    src.EndInit();
 
-            //}
-            //else
-            //{
+                    imgSig.Source = src;
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.Forms.MessageBox.Show(ex.Message);
+                }
 
-            //    // IF WACOM STU IS NOT ATTACHED: Returns message that license is invalid!
-            //    print("Signature capture error res=" + (int)res + "  ( " + res + " )");
-            //    switch (res)
-            //    {
-            //        case DynamicCaptureResult.DynCaptCancel: print("signature cancelled"); break;
-            //        case DynamicCaptureResult.DynCaptError: print("no capture service available"); break;
-            //        case DynamicCaptureResult.DynCaptPadError: print("signing device error"); break;
-            //        case DynamicCaptureResult.DynCaptNotLicensed: print("license error or device is not connected"); break;
-            //        default: print("Unexpected error code "); break;
-            //    }
-            //}
+            }
+            else
+            {
 
+                // IF WACOM STU IS NOT ATTACHED: Returns message that license is invalid!
+                print("Signature capture error res=" + (int)res + "  ( " + res + " )");
+                switch (res)
+                {
+                    case DynamicCaptureResult.DynCaptCancel: print("signature cancelled"); break;
+                    case DynamicCaptureResult.DynCaptError: print("no capture service available"); break;
+                    case DynamicCaptureResult.DynCaptPadError: print("signing device error"); break;
+                    case DynamicCaptureResult.DynCaptNotLicensed: print("license error or device is not connected"); break;
+                    default: print("Unexpected error code "); break;
+                }
+            }
+
+
+            #region MIT Libraries
 
             //if (!File.Exists(signTargetPath))
             //{
@@ -237,53 +247,76 @@ namespace TestSigCapt_WPF
             #endregion
 
 
-            #region No more used iTextSharp (incompatible license)
+            #region iTextSharp
 
-            //    //PdfReader reader = new PdfReader(@"C:\Users\visio\Desktop\LoremIpsumRight.pdf");
-            //    ////PdfReader reader = new PdfReader(@"C:\Users\visio\Desktop\LoremIpsumLeft.pdf");
-            //    //using (FileStream fs = new FileStream(@"C:\Users\visio\Desktop\Result.pdf", FileMode.Create))
-            //    //using (PdfStamper stamper = new PdfStamper(reader, fs))
-            //    //{
-            //    //    int pageNum = 1;
 
-            //    //    var strategy = new TextLocationStrategyChar(marker);
-            //    //    var processor = new PdfContentStreamProcessor(strategy);
+            string inputPdf = @"C:\Users\visio\Desktop\PDF_ManipulationTests\LoremIpsumRight.pdf";
+            string outputPdf = @"C:\Users\visio\Desktop\PDF_ManipulationTests\Result.pdf";
+            string pfxPath = @"C:\temp\firma-demo.pfx";
+            string pfxPassword = "password123";
+            string reason = "Firma di prova";
+            string location = "Italia";
 
-            //    //    var pageDic = reader.GetPageN(pageNum);
-            //    //    var resourcesDic = pageDic.GetAsDict(PdfName.RESOURCES);
-            //    //    processor.ProcessContent(ContentByteUtils.GetContentBytesForPage(reader, pageNum), resourcesDic);
+            // Load the certificate with BouncyCastle
+            Pkcs12Store pk12;
+            using (var fs = new FileStream(pfxPath, FileMode.Open, FileAccess.Read))
+            {
+                // Usa il builder per creare lo store (evita l'errore del costruttore mancante)
+                pk12 = new Pkcs12StoreBuilder().Build();
+                pk12.Load(fs, pfxPassword.ToCharArray());
+            }
 
-            //    //    strategy.FinalizeSearch();
+            string alias = null;
+            foreach (string tAlias in pk12.Aliases)
+            {
+                if (pk12.IsKeyEntry(tAlias))
+                {
+                    alias = tAlias;
+                    break;
+                }
+            }
 
-            //    //    if (strategy.MarkerPositions.Count > 0)
-            //    //    {
-            //    //        PdfContentByte canvas = stamper.GetOverContent(pageNum);
-            //    //        iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(signTargetPath);
+            var pk = pk12.GetKey(alias).Key;
+            var chain = pk12.GetCertificateChain(alias);
+            ICollection<Org.BouncyCastle.X509.X509Certificate> chainBC = new List<Org.BouncyCastle.X509.X509Certificate>();
+            foreach (var entry in chain)
+            {
+                chainBC.Add(entry.Certificate); // tipo BouncyCastle
+            }
 
-            //    //        foreach (var pos in strategy.MarkerPositions)
-            //    //        {
-            //    //            // Move of half height and width (it depends on marker position found) to center the image (high left corner) on the marker.
-            //    //            // To check X we consider an offset of 70% of the PDF itself.
-            //    //            // Values are kinda hadcoded for now, and they depend on the size of the sign canvas.
-            //    //            float resultAbsX = pos[Vector.I1] - img.Width * 0.25f;
-            //    //            float resultAbsY = pos[Vector.I2] - img.Height * 0.5f;
 
-            //    //            if (pos[Vector.I1] > reader.GetPageSize(pageNum).Width * 0.7f)
-            //    //            {
-            //    //                resultAbsX = pos[Vector.I1] - img.Width * 0.5f;
-            //    //            }
+            IExternalSignature externalSignature = new PrivateKeySignature(pk, "SHA-256");
 
-            //    //            img.SetAbsolutePosition(resultAbsX, resultAbsY);
-            //    //            canvas.AddImage(img);
-            //    //        }
-            //    //    }
-            //    //    else
-            //    //    {
-            //    //        Console.WriteLine("Marker non trovato");
-            //    //    }
-            //    //}
+            // Read the original PDF
+            iTextSharp.text.pdf.PdfReader reader = new iTextSharp.text.pdf.PdfReader(inputPdf);
+            using (FileStream os = new FileStream(outputPdf, FileMode.Create))
+            {
+                // Stamper in modalità append per non invalidare il PDF
+                PdfStamper stamper = PdfStamper.CreateSignature(reader, os, '\0', null, true);
 
-            #endregion
+                PdfSignatureAppearance appearance = stamper.SignatureAppearance;
+                appearance.Reason = reason;
+                appearance.Location = location;
+                appearance.SetVisibleSignature(new iTextSharp.text.Rectangle(100, 100, 300, 150), 1, "Signature1");
+
+                if (File.Exists(signTargetPath))
+                {
+                    var signatureImg = iTextSharp.text.Image.GetInstance(signTargetPath);
+                    appearance.SignatureGraphic = signatureImg;
+                    appearance.SignatureRenderingMode = PdfSignatureAppearance.RenderingMode.GRAPHIC_AND_DESCRIPTION;
+                }
+
+                MakeSignature.SignDetached(appearance, externalSignature, chainBC, null, null, null, 0, CryptoStandard.CADES);
+            }
+
+            Console.WriteLine("PDF firmato con successo!");
+
+
+
+            #endregion iTextSharp Signature cryptho (deprecated, use iText instead under licensing)
+
+
+            #endregion Signature INK sdk and PDF Manipulation
 
 
 
@@ -293,21 +326,21 @@ namespace TestSigCapt_WPF
 
             #region Wacom SignPro pdf API call tests (premium license is needed)
 
-            // Invocazione di Wacom SignPRO API. Richiede licenza premium.
-            // Leggi il contenuto del file JSON
-            string jsonPath = @"C:\Users\visio\Downloads\Wacom\api-demo-v4\scripts\API Command Demo\demo api autosave.txt";
-            string json = File.ReadAllText(jsonPath);
+            //// Invocazione di Wacom SignPRO API. Richiede licenza premium.
+            //// Leggi il contenuto del file JSON
+            //string jsonPath = @"C:\Users\visio\Downloads\Wacom\api-demo-v4\scripts\API Command Demo\demo api autosave.txt";
+            //string json = File.ReadAllText(jsonPath);
 
-            // Converti in Base64
-            string base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(json));
+            //// Converti in Base64
+            //string base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(json));
 
-            // Esegui Sign Pro PDF con il parametro API
-            Process.Start(
-                @"C:\Program Files (x86)\Wacom sign pro PDF\Sign Pro PDF.exe",
-                $"-api signpro:{base64}"
-            );
+            //// Esegui Sign Pro PDF con il parametro API
+            //Process.Start(
+            //    @"C:\Program Files (x86)\Wacom sign pro PDF\Sign Pro PDF.exe",
+            //    $"-api signpro:{base64}"
+            //);
 
-            #endregion
+            #endregion Wacom SignPro pdf API call tests (premium license is needed)
 
 
         }
@@ -321,5 +354,8 @@ namespace TestSigCapt_WPF
         {
             txtInfo.Text += txt + "\r\n";
         }
+
+
     }
 }
+
--- Samples/VS2022/TestSigCapt_WPF/TestSigCapt_WPF/Strategies/TextLocationStrategy.cs
+++ Samples/VS2022/TestSigCapt_WPF/TestSigCapt_WPF/Strategies/TextLocationStrategy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using iTextSharp.text.pdf.parser;
+using System.Collections.Generic;
 using System.Linq;
 
 
@@ -7,40 +8,40 @@ namespace TestSigCapt_WPF
     // Old iTextSharp tests.
 
 
-    //public class TextLocationStrategyChar : IRenderListener
-    //{
-    //    private readonly string _target;
-    //    private readonly List<(string text, Vector pos)> _chars = new List<(string, Vector)>();
-    //    public List<Vector> MarkerPositions { get; } = new List<Vector>();
+    public class TextLocationStrategyChar : IRenderListener
+    {
+        private readonly string _target;
+        private readonly List<(string text, Vector pos)> _chars = new List<(string, Vector)>();
+        public List<Vector> MarkerPositions { get; } = new List<Vector>();
 
-    //    public TextLocationStrategyChar(string target)
-    //    {
-    //        _target = target;
-    //    }
+        public TextLocationStrategyChar(string target)
+        {
+            _target = target;
+        }
 
-    //    public void RenderText(TextRenderInfo renderInfo)
-    //    {
-    //        foreach (var chrInfo in renderInfo.GetCharacterRenderInfos())
-    //        {
-    //            _chars.Add((chrInfo.GetText(), chrInfo.GetBaseline().GetStartPoint()));
-    //        }
-    //    }
+        public void RenderText(TextRenderInfo renderInfo)
+        {
+            foreach (var chrInfo in renderInfo.GetCharacterRenderInfos())
+            {
+                _chars.Add((chrInfo.GetText(), chrInfo.GetBaseline().GetStartPoint()));
+            }
+        }
 
-    //    public void BeginTextBlock() { }
-    //    public void EndTextBlock() { }
-    //    public void RenderImage(ImageRenderInfo renderInfo) { }
+        public void BeginTextBlock() { }
+        public void EndTextBlock() { }
+        public void RenderImage(ImageRenderInfo renderInfo) { }
 
-    //    public void FinalizeSearch()
-    //    {
-    //        string fullText = string.Concat(_chars.Select(c => c.text));
-    //        int index = 0;
+        public void FinalizeSearch()
+        {
+            string fullText = string.Concat(_chars.Select(c => c.text));
+            int index = 0;
 
-    //        while ((index = fullText.IndexOf(_target, index)) >= 0)
-    //        {
-    //            MarkerPositions.Add(_chars[index].pos);
-    //            index += _target.Length;
-    //        }
-    //    }
-    //}
+            while ((index = fullText.IndexOf(_target, index)) >= 0)
+            {
+                MarkerPositions.Add(_chars[index].pos);
+                index += _target.Length;
+            }
+        }
+    }
 
 }
