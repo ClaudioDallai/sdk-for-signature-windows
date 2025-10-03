43f7c168e346d44c15d222e701a258637e9cd88f
Claudio Dallai <149816791+ClaudioDallai@users.noreply.github.com>
2025-10-03 10:17:03 +0200

    PDFSharp.Signatures implementation

--- Samples/VS2022/TestPDFSharpSignatures/TestPDFSharpSignatures/MainWindow.xaml.cs
+++ Samples/VS2022/TestPDFSharpSignatures/TestPDFSharpSignatures/MainWindow.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System.Text;
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Pdf.Security;
+using PdfSharp.Pdf.Signatures;
+using System.Security.Cryptography.Pkcs;
+using System.Security.Cryptography.X509Certificates;
+
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,11 +16,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-using PdfSharp.Pdf;
-//using PdfSharp.Cryptography;
-using System.Security.Cryptography.Pkcs;   // from NuGet
-using System.Security.Cryptography.X509Certificates;
 
 
 namespace TestPDFSharpSignatures
@@ -25,6 +28,20 @@ namespace TestPDFSharpSignatures
         public MainWindow()
         {
             InitializeComponent();
+
+            var document = PdfReader.Open("file.pdf", "password");
+            PdfPage page = document.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            var pdfPosition = gfx.Transformer.WorldToDefaultPage(new XPoint(144, 600));
+            var options = new DigitalSignatureOptions
+            {
+                ContactInfo = "John Doe",
+                Location = "Seattle",
+                Reason = "License Agreement",
+                Rectangle = new XRect(pdfPosition.X, pdfPosition.Y, 200, 50),
+                //AppearanceHandler = new SignatureAppearanceHandler()
+            };
         }
     }
 }
